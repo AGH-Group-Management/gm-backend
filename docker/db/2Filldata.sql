4d1846c49928e53bcdd136869f12a3e628ecc580
Xavvee <hkabzinski@studnet.agh.edu.pl>
2024-03-20 19:46:43 +0100

    Adding mock data in progress.

--- docker/db/2Filldata.sql
+++ docker/db/2Filldata.sql
@@ -30,49 +30,72 @@ INSERT into users (name, surname, email, roleid) VALUES ('Marek', 'Gajęcki', 'm
 --- DZIEKANS
 INSERT INTO dziekans (dziekanid) values (1), (7);
 
+
+--- PATHS
+insert into paths (name) values ('Wytwarzania Oprogramowania'), ('Algorytmiczna'), ('Algorytmiczno - aplikacyjna');
+
 --- STUDENTS
-INSERT INTO  students (studentid, semester, index) values (2, 4, '410800'),(16,4, '410813'),
-                                                          (3, 2, '410801'),(17,6, '320816'),
-                                                          (4, 4, '410809'),(18,2, '310819'),
-                                                          (5, 8, '310818'),(19,8, '310819'),
-                                                          (6, 6, '398213'),(20,2, '452312'),
-                                                          (8, 6, '387213'),(21,4, '432143'),
-                                                          (9, 10, '298712'),(22,4, '423145'),
-                                                          (10, 2, '463412'),(23,4, '419750'),
-                                                          (11, 4, '420841'),
-                                                          (12, 6, '407271'),
-                                                          (13, 8, '321358'),
-                                                          (14, 2, '431267'),
-                                                          (15, 2, '459281');
+INSERT INTO  students (studentid, semester, pathid, index) values (2, 4, 1, '410800'),(16,4, 2, '410813'),
+                                                          (3, 2, 1, '410801'),(17,6, 3, '320816'),
+                                                          (4, 4, 2, '410809'),(18,2, 3, '310819'),
+                                                          (5, 8, 3, '310818'),(19,8, 2, '310819'),
+                                                          (6, 6, 3, '398213'),(20,2, 1, '452312'),
+                                                          (8, 6, 2, '387213'),(21,4, 2, '432143'),
+                                                          (9, 10, 3, '298712'),(22,4,3, '423145'),
+                                                          (10, 2, 2, '463412'),(23,4,1, '419750'),
+                                                          (11, 4, 1, '420841'),
+                                                          (12, 6, 1, '407271'),
+                                                          (13, 8, 2, '321358'),
+                                                          (14, 2, 3, '431267'),
+                                                          (15, 2, 1, '459281');
 
 
 --- SUBJECTS
 
-INSERT INTO subjects (name, description, startdate, enddate, isrequired, maxslots) VALUES
-                                                                                       ('Logika matematyczna', 'Aby Studenci rozumieli podstawy logiki i celowość jej stosowania - na choćby elementarnym poziomie..','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Algorytmy i struktury danych', 'Przedmiot zapoznaje studentów z podstawowymi algorytmami, strukturami danych, oraz metodami tworzenia i analizy algorytmów.','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Fizyka 1', 'Zapoznanie studentów z najbardziej fundamentalnymi prawami opisującymi budowę i właściwości materii oraz oddziaływaniami obserwowanymi w przyrodzie.','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Technika cyfrowa', 'Moduł omawia budowę i działanie podstawowych układów cyfrowych.','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Systemy operacyjne', 'Zapoznanie studentów z zasadami funkcjonowania systemów operacyjnych oraz metodami ich budowy.','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Metody obliczeniowe w nauce i technice', 'Celem modułu jest zapoznanie studenta z podstawowymi algorytmami numerycznymi.','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Architektura komputerów', 'W trakcie zajęć studenci poznają podstawowe zagadnienia budowy komputerów o wielu procesorach oraz problemy związane z ich wykorzystaniem.','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Inżynieria oprogramowania', '	Celem zajęć jest przekazanie studentowi wiedzy o współczesnych metodykach i technikach stosowanych w zespołach programistycznych.','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Systemy rozproszone', 'Moduł wprowadza studenta w podstawy systemów rozproszonych.','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Bezpieczeństwo sieci komputerowych', 'Przekazanie wiedzy o technikach monitorowania stanu urządzeń sieciowych i ich zabezpieczania.','2024-03-01', '2024-06-30', false, 112),
-                                                                                       ('Systemy czasu rzeczywistego', 'Celem kształcenia jest przekazanie wiedzy dotyczącej i analizy.','2024-03-01', '2024-06-30', false, 112),
-                                                                                       ('Programowanie w języku Icon', 'Celem przedmiotu jest zapoznanie studentów z programowaniem w języku Icon.','2024-03-01', '2024-06-30', false, 112),
-                                                                                       ('Programowanie w języku Scala', '','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna1', '','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna2', '.','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna3', '','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna4', '','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna5', '','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna6', '','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna7', '','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna8', '','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna9', '','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna0', '','2024-03-01', '2024-06-30', true, null),
-                                                                                       ('Logika matematyczna-', '','2024-03-01', '2024-06-30', true, null);
+INSERT INTO subjects (name, description, semester, isrequired, maxslots) VALUES
+                                                                                       ('Logika matematyczna', 'Aby Studenci rozumieli podstawy logiki i celowość jej stosowania - na choćby elementarnym poziomie..',2, true, null),
+                                                                                       ('Algorytmy i struktury danych', 'Przedmiot zapoznaje studentów z podstawowymi algorytmami, strukturami danych, oraz metodami tworzenia i analizy algorytmów.',2, true, null),
+                                                                                       ('Fizyka 1', 'Zapoznanie studentów z najbardziej fundamentalnymi prawami opisującymi budowę i właściwości materii oraz oddziaływaniami obserwowanymi w przyrodzie.',2, true, null),
+                                                                                       ('Technika cyfrowa', 'Moduł omawia budowę i działanie podstawowych układów cyfrowych.',4, true, null),
+                                                                                       ('Systemy operacyjne', 'Zapoznanie studentów z zasadami funkcjonowania systemów operacyjnych oraz metodami ich budowy.',4, true, null),
+                                                                                       ('Metody obliczeniowe w nauce i technice', 'Celem modułu jest zapoznanie studenta z podstawowymi algorytmami numerycznymi.',4, true, null),
+                                                                                       ('Architektura komputerów', 'W trakcie zajęć studenci poznają podstawowe zagadnienia budowy komputerów o wielu procesorach oraz problemy związane z ich wykorzystaniem.',6, true, null),
+                                                                                       ('Inżynieria oprogramowania', '	Celem zajęć jest przekazanie studentowi wiedzy o współczesnych metodykach i technikach stosowanych w zespołach programistycznych.',6, true, null),
+                                                                                       ('Systemy rozproszone', 'Moduł wprowadza studenta w podstawy systemów rozproszonych.',6, true, null),
+                                                                                       ('Bezpieczeństwo sieci komputerowych', 'Przekazanie wiedzy o technikach monitorowania stanu urządzeń sieciowych i ich zabezpieczania.',6, false, 112),
+                                                                                       ('Systemy czasu rzeczywistego', 'Celem kształcenia jest przekazanie wiedzy dotyczącej i analizy.',6, false, 112),
+                                                                                       ('Programowanie w języku Icon', 'Celem przedmiotu jest zapoznanie studentów z programowaniem w języku Icon.',4, false, 112),
+                                                                                       ('Programowanie w języku Scala', 'Wprowadzenie do Scali obejmujące elementy programowania obiektowego, generycznego i funkcyjnego.',4, true, 112),
+                                                                                       ('Elementy statystycznego uczenia maszynowego', 'Niniejszy kurs stanowi wprowadzenie do metod i algorytmów statystycznego uczenia maszynowego',4, true, null),
+                                                                                       ('Wprowadzenie do inżynierii bezpieczeństwa', 'Moduł prezentuje przekrojową i aktualną wiedzę z dziedziny inżynierii bezpieczeństwa.',6, true, null),
+                                                                                       ('Podstawy uczenia maszynowego', 'Celem jest zapoznanie studentów z głównymi algorytmami uczenia maszynowego i inżynierii cech.',6, false, 112),
+                                                                                       ('Eksploracja danych', 'Eksploracja danych jako proces.',8, false, null),
+                                                                                       ('Środowiska udostępniania usług', 'Tematyka modułu dotyczy budowy usług eksploatowanych w systemach chmurowych.',8, false, null),
+                                                                                       ('Programowanie animacji', 'Celem jest zapoznanie studentów z animacją komputerową',8, false, null),
+                                                                                       ('Uczenie paszyn', 'Celem zajęć jest przekazanie wiedzy studentom w obszarze uczenia paszyn.',8, false, 112),
+                                                                                       ('Kompetencje w zmieniającym się społeczeństwie', 'Kompetencje w zmieniającym się społeczeństwie to przedmiot!',10, false, null),
+                                                                                       ('Problemy prawne systemów AI', 'Celem kursu jest wprowadzenie studentów do problematyki prawnych aspektów wiążących się z projektowaniem.',10, false, null),
+                                                                                       ('Informatyka medyczna', 'Przedmiot zapoznaje z zagadnieniami z dziedziny informatyki medycznej.',10, false, null),
+                                                                                       ('Ochrona własności intelektualnej', 'Celem modułu jest przedstawienie, co to jest własność intelektualna.',2, true, null);
 
 
----
+--- STUDENTS TO SUBJECTS
+insert into studentstosubjects (studentsid, subjects_id) VALUES (1, 4), (1, 5),(1, 6),(1, 12),(3, 1),(3, 2),(3, 3),(6, 1),(6, 2),(6, 3),(10, 1),(10, 2),(10, 3),(15, 1),(15, 2),(15, 3),(20, 1),(20, 2),(20, 3),(21, 1),(21, 2),(21, 3),
+                                                                (5, 4), (5, 5),(5, 6),(5, 13),(2, 4), (2, 5),(2, 6),(2, 13),(12, 4), (12, 5),(12, 6),(12, 12),(14, 4), (14, 5),(14, 6),(14, 13),(16, 4), (16, 5),(16, 6),(16, 12),(17, 4), (17, 5),(17, 6),(17, 12),
+                                                                (6, 7), (6, 8),(6, 9),(6,10),(8, 7), (8, 8),(8, 9),(8,11),(12, 7), (12, 8),(12, 9),(12,11),(17, 7), (17, 8),(17, 9),(17,10),(12,15),
+                                                                (7,17), (7,18), (7,19), (19,17), (19,18), (19,19),(8,17), (8,18), (8,19),(8,20),
+                                                                (13, 21), (13, 22), (13, 23);
+
+
+
+--- VACATION REQUESTS
+
+
+--- REQUESTS
+
+
+--- CHANGE REQUEST
+
+
+
+--- OTHER REQUEST
