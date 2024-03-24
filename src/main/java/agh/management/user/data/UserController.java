7dfdeb984d50f7c91b73e0e07d77c6e30d5eeba1
Kacper <kkorta@student.agh.edu.pl>
2024-03-24 14:01:16 +0100

    Added login handler server

--- src/main/java/agh/management/user/data/UserController.java
+++ src/main/java/agh/management/user/data/UserController.java
@@ -1,2 +1,39 @@
-package agh.management.user.data;public class UserController {
+package agh.management.user.data;
+
+import agh.management.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Base64;
+
+class Credentials{
+    String userName;
+    String password;
+
+    public Credentials(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+}
+@RestController
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+    @RequestMapping("/login")
+    public boolean login(@RequestBody Credentials credentials ) {
+        User user1 = userRepository.findByEmail(credentials.userName);
+        return user1.getPassword().equals(credentials.password);
+    }
+
+    @RequestMapping("/user")
+    public Principal user(HttpServletRequest request) {
+        String authToken = request.getHeader("Authorization")
+                .substring("Basic".length()).trim();
+        return () ->  new String(Base64.getDecoder()
+                .decode(authToken)).split(":")[0];
+    }
 }
